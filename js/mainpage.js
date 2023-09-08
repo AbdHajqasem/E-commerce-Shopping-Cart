780a83d807e5b5cb63634045217a32e6f4759ecc
Abed <abed.hajqasem@hotmail.com>
2023-09-08 20:17:51 +0300

    new

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,5 +1,6 @@
 let arrItem;
-let shoppingCart=[];
+let shoppingCart=JSON.parse(localStorage.getItem("additem"))||[];
+console.log(shoppingCart);
 async function products() {
     try {
         let items = await fetch('https://fakestoreapi.com/products');
@@ -9,8 +10,8 @@ async function products() {
         arrItem.forEach(element => {
             container.innerHTML += `
                 <div class=" col-lg-3 col-md-4 col-11" id="items">
-                    <div class="row">
-                        <div class="col-12" id="imgcontainer">
+                    <div class="row justify-content-center">
+                        <div class="col-11" id="imgcontainer">
                             <img src="${element.image}" alt="" >
                         </div>
                         <div class="col-12">
@@ -35,9 +36,17 @@ async function products() {
 products();
 
 function addToCart(id){
+shoppingCart=JSON.parse(localStorage.getItem("additem"))||[]; 
   let addedItems=arrItem.filter((e)=>e.id===id)
-  shoppingCart=[...shoppingCart, addedItems];
+
+  if(shoppingCart.findIndex((e)=>e[0].id===id)>=0)
+  {
+    return;
+  }  
+  
+  shoppingCart.push(addedItems);
   localStorage.setItem("additem",JSON.stringify(shoppingCart));
+  
 }
 
 
--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,11 +1,15 @@
 let container=document.getElementById("con");
-let addedItems=localStorage.getItem('additem');
-if(addedItems){
-    let itemsInCart=JSON.parse(addedItems);
-    products(itemsInCart);
-}
-function products(itemsInCart){
+let totalPriceElement=document.getElementById("totalprice");
+let totalPrice=0.0;
+function products(){
+    let itemsInCart=JSON.parse(localStorage.getItem('additem'));
+    if(!itemsInCart||itemsInCart.length==0){
+        container.innerHTML="no item";
+        return;
+    }
     itemsInCart.forEach(element => {
+    totalPrice+=element[0].price;
+    totalPriceElement.innerText=totalPrice+"$";
      container.innerHTML+=
      `<div class="row align-items-center bg-danger" style="margin:5px 0 5px 0 ;">
         <div class="col-3">
@@ -17,7 +21,9 @@ function products(itemsInCart){
                     <p>${element[0].title}</p>
                 </div>
                 <div class="col-md-2 col-5 ">
-                    <i class="fa-solid fa-trash"></i>
+                <button id="remove-${element[0].id}" onclick=removeItem(${element[0].id})>
+                    <i class="fa-solid fa-trash" id="removeicon"></i>
+                    </button>
                 </div>
             </div>
             <div class="row justify-content-between">
@@ -25,31 +31,56 @@ function products(itemsInCart){
                     <p id="price-${element[0].id}">${element[0].price}$</p>
                 </div>
                 <div class="col-md-2 col-5 d-flex align-items-center justify-content-between" id="divcounter">
-                    <button style="display: flex; align-items: center; justify-content: center;" class="counterbtn "onclick=dencrease(${element[0].id})>-</button>
+                    <button style="display: flex; align-items: center; justify-content: center;" class="counterbtn "onclick=dencrease(${element[0].id},${element[0].price})>-</button>
                     <input type="text" value="1" id="${element[0].id}" class="countnum">
-                    <button style="display: flex; align-items: center; justify-content: center;" class="counterbtn " onclick=increase(${element[0].id}) >+</button>
+                    <button style="display: flex; align-items: center; justify-content: center;" class="counterbtn " onclick=increase(${element[0].id},${element[0].price}) >+</button>
                 </div>
             </div>
         </div>
     </div>` 
     });
 }
-function increase(id){
+products();
+function increase(id,firstPrice){
    let counter= document.getElementById(id);
    let plus=parseInt(counter.value)+1;
    counter.value=plus;
    let price=document.getElementById(`price-${id}`);
-   let newPrice=parseFloat(price.innerText)/plus;
-   newPrice=parseFloat(price.innerText)+newPrice;
-   price.innerText=newPrice;
+   let newPrice=parseFloat(price.innerText)+firstPrice;
+   let newTotalprice=parseFloat(totalPriceElement.innerText)+firstPrice;
+   let newTotalprice1=newTotalprice.toFixed(2);
+   price.innerText=newPrice +"$";
+   totalPriceElement.innerText=newTotalprice1+"$";
+
+   
 }
-function dencrease(id){
+function dencrease(id,firstPrice){
     let counter=document.getElementById(id);
     if(parseInt(counter.value)>1){
     let minus=parseInt(counter.value)-1;
     counter.value=minus;
-
+    let price=document.getElementById(`price-${id}`);
+    let newPrice=parseFloat(price.innerText)-firstPrice;
+    let newPrice1=newPrice.toFixed(2);
+    let newTotalprice=parseFloat(totalPriceElement.innerText)-firstPrice;
+    let newTotalprice1=newTotalprice.toFixed(2);
+    price.innerText=newPrice1+"$";
+    totalPriceElement.innerText=newTotalprice1+"$";
     }
 }
+
+function removeItem(id) {
+    let price=document.getElementById(`price-${id}`);
+    let currentTotalPrice=parseFloat(totalPriceElement.innerText)-parseFloat(price.innerText);
+    console.log(currentTotalPrice);
+    totalPriceElement.innerText=currentTotalPrice+"$";
+    let itemsInCart = JSON.parse(localStorage.getItem('additem'));
+    itemsInCart = itemsInCart.filter(element => element[0].id !== id);
+    localStorage.setItem('additem', JSON.stringify(itemsInCart));
+    container.innerHTML="";
+    products();
+}
+
+
 
 
